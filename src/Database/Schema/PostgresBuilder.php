43eba6bda01221a151b0d729d52da8c40c98e873
sachaBlitz <36762905+sachaBlitz@users.noreply.github.com>
2018-08-01 15:55:47 +0200

    Update PostgresBuilder.php

--- src/Database/Schema/PostgresBuilder.php
+++ src/Database/Schema/PostgresBuilder.php
@@ -2,28 +2,14 @@
 
 namespace ElevenLab\GeoLaravel\Database\Schema;
 
-class PostgresBuilder extends \Illuminate\Database\Schema\Builder
-{
-
-    use GeoBuilder;
-    /**
-     * Determine if the given table exists.
-     *
-     * @param  string  $table
-     * @return bool
-     */
-    public function hasTable($table)
+if(version_compare(app()->version(), "5.2.31") < 0){
+    class PostgresBuilder extends \Illuminate\Database\Schema\Builder
     {
-        if (is_array($schema = $this->connection->getConfig('schema'))) {
-            $schema = head($schema);
-        }
-
-        $schema = $schema ? $schema : 'public';
-
-        $table = $this->connection->getTablePrefix().$table;
-
-        return count($this->connection->select(
-                $this->grammar->compileTableExists(), [$schema, $table]
-            )) > 0;
+        use GeoBuilder;
+    }
+}else{
+    class PostgresBuilder extends \Illuminate\Database\Schema\PostgresBuilder
+    {
+        use GeoBuilder;
     }
 }
